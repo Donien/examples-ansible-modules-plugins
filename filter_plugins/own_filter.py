613dd06a75f35d8231c7dc66eeb9ed467bfcc33a
Donien <donien.96@hotmail.com>
2023-03-29 21:06:31 +0200

    Add mockery

--- filter_plugins/own_filter.py
+++ filter_plugins/own_filter.py
@@ -7,19 +7,41 @@ __metaclass__ = type
 from ansible.errors import AnsibleFilterError
 from ansible.module_utils.common.text.converters import to_text
 
-def hello_world(greeting):
-    greeting = greeting + ' world!'
-    return to_text(greeting)
+import datetime
+import time
+
+def to_unixtime(time_string, format="%Y-%m-%d %H:%M:%S"):
+    raise ValueError("Dingo bingo bongo ding")
+    time_object = datetime.datetime.strptime(time_string, format)
+    time_object = time.mktime(time_object.timetuple())
+    return to_text(time_object)
 
 def second_power(number):
     number = number * number
     return to_text(number)
 
+def mocking_meme(st):
+    if type(st) != str:
+        raise TypeError(f"Value '{st}' not of type string...")
+    st_arr = []
+    mock_count = 0
+    for letter in st:
+        if mock_count%2 == 0:
+            st_arr.append(letter.lower())
+        else:
+            st_arr.append(letter.upper())
+        if not letter.isalpha():
+            mock_count = 0
+        else:
+            mock_count += 1
+            
+    st = "".join(st_arr)
+    return to_text(st)
+
 class FilterModule(object):
     def filters(self):
         return {
-            'hello_world': hello_world,
+            'to_unixtime': to_unixtime,
+            'mocking': mocking_meme,
             'second_power': second_power
         }
-
-
